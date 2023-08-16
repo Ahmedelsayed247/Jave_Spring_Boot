46a35bda73a04b56402da4f52b37e88bdf926922
Ahmedelsayed247 <manahmed247@gmail.com>
2023-08-17 02:28:54 +0300

    add Qualifiers,Primary,lazy-initialization

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/core/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.core.springcoredemo.rest;
+
+import com.core.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController
+{
+    //define a private field for the dependence
+   private Coach Mycoach ;
+
+   // define a constructor for dependence injection
+    @Autowired
+    public void setCoach(Coach theCoach){
+       Mycoach = theCoach;
+    }
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return Mycoach.getDailyWorkout() ;
+    }
+}
