f24464e41bdec98e2381b84e3dfcd9d367d88db5
Ahmedelsayed247 <manahmed247@gmail.com>
2023-09-03 02:20:35 +0300

    crud-employee -with-spring-data-rest

---
+++ 04-Spring-boot-Rest-CRUD/04-spring-boot-rest-crud-employee -with-spring-data-rest/src/main/java/com/springBoot/employee/crudDemoemployee/CrudDemoEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.employee.crudDemoemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoEmployeeApplication.class, args);
+	}
+
+}
